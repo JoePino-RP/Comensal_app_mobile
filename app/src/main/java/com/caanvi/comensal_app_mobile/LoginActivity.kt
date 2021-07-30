0653cf621914fcd68917ed1a2db9320d42f35440
jarciniegasnino <jonarni_0699@hotmail.com>
2021-07-30 13:25:57 -0500

    Nuevos campos Login-registro
    Cambios en el campo login y registro, junto con el boton de cerrar

--- app/src/main/java/com/caanvi/comensal_app_mobile/LoginActivity.kt
+++ app/src/main/java/com/caanvi/comensal_app_mobile/LoginActivity.kt
@@ -1,15 +1,19 @@
 package com.caanvi.comensal_app_mobile
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+
 import android.os.Bundle
-import android.widget.Button
-import android.widget.ImageButton
+import androidx.appcompat.app.AppCompatActivity
+
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+
+
     }
+
+
+
 }
---
+++ app/src/main/java/com/caanvi/comensal_app_mobile/OlvideActivity.kt
@@ -0,0 +1,15 @@
+package com.caanvi.comensal_app_mobile
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
+
+class OlvideActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_olvide)
+
+    }
+}
