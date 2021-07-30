11a959ee44df21f655e9210ea993ce3d12f69ea0
jarciniegasnino <jonarni_0699@hotmail.com>
2021-07-30 09:14:58 -0500

    Vistas-Login,registro,olvide contraseña
    Vista Login- boton olvide mi contrasena,
    Vista registro
    vista recuperar contrasena

--- app/src/main/java/com/caanvi/comensal_app_mobile/RegisterActivity.kt
+++ app/src/main/java/com/caanvi/comensal_app_mobile/RegisterActivity.kt
@@ -1,15 +1,11 @@
 package com.caanvi.comensal_app_mobile
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.ImageButton
 
-class LoginActivity : AppCompatActivity() {
+class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
-
+        setContentView(R.layout.activity_register)
     }
 }
