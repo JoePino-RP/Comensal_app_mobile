24117a156ba2c09c2db52793090785c202943f50
joelrammstein <joelrammstein@gmail.com>
2021-08-02 14:26:39 -0500

    Cambios Joel, imgs

--- app/src/main/java/com/caanvi/comensal_app_mobile/Login/Activities/PrincipalFragments.kt
+++ app/src/main/java/com/caanvi/comensal_app_mobile/Login/Activities/PrincipalFragments.kt
@@ -2,11 +2,46 @@ package com.caanvi.comensal_app_mobile.Login.Activities
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.caanvi.comensal_app_mobile.Login.fragments.DashboardFragment
+import com.caanvi.comensal_app_mobile.Login.fragments.LoginFragment
+import com.caanvi.comensal_app_mobile.Login.fragments.SettingsFragment
 import com.caanvi.comensal_app_mobile.R
+import com.caanvi.comensal_app_mobile.databinding.ActivityPrincipalFragmentsBinding
 
 class PrincipalFragments : AppCompatActivity() {
+
+    private val dashboardFragment = DashboardFragment()
+    private val loginFragment = LoginFragment()
+    private val settingsFragment = SettingsFragment()
+
+    private lateinit var binding: ActivityPrincipalFragmentsBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_principal_fragments)
+
+        replaceFragment(dashboardFragment)
+        binding.bottomNavigation.setOnNavigationItemReselectedListener {
+            when (it.itemId){
+                R.id.ic_dashboard -> replaceFragment(dashboardFragment)
+                R.id.ic_settings -> replaceFragment(settingsFragment)
+                R.id.ic_info -> replaceFragment(loginFragment)
+
+            }
+            true
+        }
+
+
+
+    }
+
+
+    private fun replaceFragment(fragment: Fragment){
+        if (fragment !=null){
+            val transaction = supportFragmentManager.beginTransaction()
+            transaction.replace(R.id.fragment_container, fragment)
+            transaction.commit()
+        }
     }
 }
