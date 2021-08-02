66264c1a4e5161fbe832b5bbb8eab4f1bd6fba99
joelrammstein <joelrammstein@gmail.com>
2021-08-02 11:36:26 -0500

    Cambios Joel, Crear Actividad que contenga Fragments

---
+++ app/src/main/java/com/caanvi/comensal_app_mobile/Login/Activities/PrincipalFragments.kt
@@ -0,0 +1,12 @@
+package com.caanvi.comensal_app_mobile.Login.Activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.caanvi.comensal_app_mobile.R
+
+class PrincipalFragments : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_principal_fragments)
+    }
+}
