0debe6a54fe00b4875c95a4e776c8739b6f5a406
jarciniegasnino <jonarni_0699@hotmail.com>
2021-08-05 14:28:41 -0500

    Gustos Comensal
    Añadido activity

--- app/src/main/java/com/caanvi/comensal_app_mobile/Login/Activities/GustosComensal.kt
+++ app/src/main/java/com/caanvi/comensal_app_mobile/Login/Activities/GustosComensal.kt
@@ -1,5 +1,6 @@
 package com.caanvi.comensal_app_mobile.Login.Activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.caanvi.comensal_app_mobile.R
@@ -8,5 +9,18 @@ class GustosComensal : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_gustos_comensal)
+
     }
+
+    //Funcion para regresar a la actividad anterior y que no se cierre la app presionando el boton hacia atras
+    override fun onBackPressed() {
+        super.onBackPressed()
+        //codigo adicional
+        val intent = Intent(applicationContext, MenuActi::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        startActivity(intent)
+        finish()
+    }
+
+
 }
