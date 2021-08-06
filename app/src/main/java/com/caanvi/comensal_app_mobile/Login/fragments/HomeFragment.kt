d799bd723ebefd74bc4c894ebe7c7ea8f2f3edba
joelrammstein <joelrammstein@gmail.com>
2021-08-05 23:49:01 -0500

    Cambios Joel, drawable, fragments

--- app/src/main/java/com/caanvi/comensal_app_mobile/Login/fragments/HomeFragment.kt
+++ app/src/main/java/com/caanvi/comensal_app_mobile/Login/fragments/HomeFragment.kt
@@ -1,31 +1,134 @@
 package www.sanju.customtabbar.Fragments
 
 
+
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import com.caanvi.comensal_app_mobile.R
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.caanvi.comensal_app_mobile.Login.Api.RetrofitClient
+import com.caanvi.comensal_app_mobile.Login.Modals.Restaurant
+import com.caanvi.comensal_app_mobile.Login.Modals.RestaurantResponse
+import com.caanvi.comensal_app_mobile.Login.RecyclerView.GetRestaurant.RestaurantAdapter
+import com.caanvi.comensal_app_mobile.databinding.FragmentHomeBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 /**
  * A simple [Fragment] subclass.
  */
 class HomeFragment : Fragment() {
 
+   // ASI SE UTILIZA EL BINDIN EN FRGMENTS
+    private  var _binding : FragmentHomeBinding? = null
+    private val binding get()= _binding!!
+    /////////////////////////////////////////
+
+    private lateinit var adapter: RestaurantAdapter
+    val restaurantList = mutableListOf<Restaurant>()
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        // ASI SE UTILIZA EL BINDIN EN FRGMENTS, se cambia algo de la linea de codigo en esta parte
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+
+        getRestaurant()
+        initRecyclerRestaurant()
+
+        return binding.root
+////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+
+    }
+
+
+
+    fun initRecyclerRestaurant(){
+        binding.recyclerRestaurant.layoutManager = LinearLayoutManager(requireActivity().applicationContext)
+        adapter = RestaurantAdapter(restaurantList)
+        binding.recyclerRestaurant.adapter = adapter
+    }
+
+    fun initRecyclerRestaurant1(){
+        binding.recyclerView.layoutManager = LinearLayoutManager(requireActivity().applicationContext)
+        adapter = RestaurantAdapter(restaurantList)
+        binding.recyclerView.adapter = adapter
+    }
+
+
+
+    fun getRestaurant(){
+        RetrofitClient.instance.getRestaurant()
+            .enqueue(object: Callback<RestaurantResponse> {
+
+                override fun onFailure(call: Call<RestaurantResponse>, t: Throwable) {
+                    Toast.makeText(requireActivity().applicationContext, t.message, Toast.LENGTH_LONG).show()
+                }
+
+                override fun onResponse(call: Call<RestaurantResponse>, response: Response<RestaurantResponse>) {
+
+                    if(response.body()?.conecto!!){
+
+                        val restaurantGot : RestaurantResponse? = response.body()
+                        val addRestaurant = restaurantGot?.restaurant?: emptyList()
+                        restaurantList.clear()
+                        restaurantList.addAll(addRestaurant)
+                        adapter.notifyDataSetChanged()
+
+                    }else{
+
+                        //Toast.makeText(applicationContext, response.body()?.message, Toast.LENGTH_LONG).show()
+                        Toast.makeText(requireActivity().applicationContext, "NO hay restaurantes", Toast.LENGTH_LONG).show()
+                    }
+                }
+            })
+    }
+
+    fun getRestaurant1(){
+        RetrofitClient.instance.getRestaurant()
+            .enqueue(object: Callback<RestaurantResponse> {
+
+                override fun onFailure(call: Call<RestaurantResponse>, t: Throwable) {
+                    Toast.makeText(activity?.applicationContext, t.message, Toast.LENGTH_LONG).show()
+                }
+
+                override fun onResponse(call: Call<RestaurantResponse>, response: Response<RestaurantResponse>) {
+
+                    if(response.body()?.conecto!!){
+
+                        val restaurantGot : RestaurantResponse? = response.body()
+                        val addRestaurant = restaurantGot?.restaurant?: emptyList()
+                        restaurantList.clear()
+                        restaurantList.addAll(addRestaurant)
+                        adapter.notifyDataSetChanged()
+
+                    }else{
+
+                        //Toast.makeText(applicationContext, response.body()?.message, Toast.LENGTH_LONG).show()
+                        Toast.makeText(activity?.applicationContext, "NO hay restaurantes", Toast.LENGTH_LONG).show()
+                    }
+                }
+            })
     }
 
 
 
 
+
 }
 
-private fun Fragment.onBackPressed() {
-    TODO("Not yet implemented")
-}
+
+
+
