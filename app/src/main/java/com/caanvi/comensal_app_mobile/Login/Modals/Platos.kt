4bd933896607a24aecd9640320420291aba1d485
joelrammstein <joelrammstein@gmail.com>
2021-08-12 09:52:19 -0500

    Cambios Joel, buscar en fragments

--- app/src/main/java/com/caanvi/comensal_app_mobile/Login/Modals/Platos.kt
+++ app/src/main/java/com/caanvi/comensal_app_mobile/Login/Modals/Platos.kt
@@ -2,9 +2,9 @@ package com.caanvi.comensal_app_mobile.Login.Modals
 
 import java.io.Serializable
 
-data class Platos (var id_pla :String,
-                   var nombre_pla : String,
-                   var imagen_pla: String,
-                   var precio_pla: String,
-                   var descripcion_pla: String,
+data class Platos (var id_platos :String,
+                   var nombre_plato : String,
+                   var imagen_plato: String,
+                   var precio_plato: String,
+                   var descripcion_plato: String,
                   ) : Serializable
--- app/src/main/java/com/caanvi/comensal_app_mobile/Login/RecyclerView/GetPlatos/PlatosAdapter.kt
+++ app/src/main/java/com/caanvi/comensal_app_mobile/Login/RecyclerView/GetPlatos/PlatosAdapter.kt
@@ -7,6 +7,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.caanvi.comensal_app_mobile.Login.Modals.Platos
 import com.caanvi.comensal_app_mobile.R
 import com.caanvi.comensal_app_mobile.databinding.PlatosBinding
+
 import com.squareup.picasso.Picasso
 
 class PlatosAdapter (private val _restaurant:List<Platos>, val onClickListener: OnClickListener):  RecyclerView.Adapter<PlatosAdapter.PlatosViewHolder>() {
@@ -22,15 +23,16 @@ class PlatosAdapter (private val _restaurant:List<Platos>, val onClickListener: 
     }
 
     override fun onBindViewHolder(holder: PlatosViewHolder, position: Int) {
+        //var binding = PlatosBinding.bind(holder.itemView)
         var binding = PlatosBinding.bind(holder.itemView)
 
 
-        //Añadi el texto que identifica a la información
-        binding.txtNombrePlato.text = "Nombre: "+ _restaurant[position].nombre_pla
-        binding.txtPrecioPlato.text = "Precio: "+_restaurant[position].precio_pla
-        binding.txtDescripcionPlato.text ="Descripcion: "+ _restaurant[position].descripcion_pla
+            //Añadi el texto que identifica a la información
+        binding.txtNombrePlato.text = "Nombre: "+ _restaurant[position].nombre_plato
+        binding.txtPrecioPlato.text = "Precio: "+_restaurant[position].precio_plato
+        binding.txtDescripcionPlato.text ="Descripcion: "+ _restaurant[position].descripcion_plato
        // binding.txtTelefono.text ="Teléfono: "+ _restaurant[position].
-        Picasso.get().load(_restaurant[position].imagen_pla).into(binding.imgRestaurant)
+        Picasso.get().load(_restaurant[position].imagen_plato).into(binding.imgRestaurant)
 
 
     }
